ec4df8cf8e1be1b5466e32ec4275c3d0a1da1454
mob35 <toonelicious@gmail.com>
2017-11-28 22:05:11 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,53 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { OneSignal } from '@ionic-native/onesignal';
 
 import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    private oneSignal: OneSignal
+  ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      if (platform.is('cordova')) {
+        this.onSignalSetup();
+      }
     });
+  }
+  onSignalSetup() {
+    this.oneSignal.startInit('4deb2817-a4ea-496c-867e-9eb26940f565', '503984043648');
+
+    // this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
+
+    this.oneSignal.handleNotificationReceived().subscribe((onReceived) => {
+      // do something when notification is received
+      let notifications = window.localStorage.getItem('buyerNotification') ? JSON.parse(window.localStorage.getItem('buyerNotification')) : [];
+
+      notifications.unshift({
+        date: new Date(),
+        message: onReceived.payload.body
+      });
+
+      window.localStorage.setItem('buyerNotification', JSON.stringify(notifications));
+    });
+
+    this.oneSignal.handleNotificationOpened().subscribe(() => {
+      // do something when a notification is opened
+    });
+
+    this.oneSignal.endInit();
   }
 }
 
